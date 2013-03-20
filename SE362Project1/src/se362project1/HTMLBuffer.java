f52a5472936baca129f5e9614de0b59c345810a3
RGood <Randy@Randy-PC.(none)>
2013-03-20 16:10:57 -0400

    Creat FormatCheck interface and HTMLBuffer class

---
+++ SE362Project1/src/se362project1/HTMLBuffer.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se362project1;
+
+/**
+ *
+ * @author Randy
+ */
+public class HTMLBuffer {
+    
+}
