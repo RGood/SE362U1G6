29f2fe30dbe861b1191d171acb4ee9ab0d5736da
RGood <Randy@Randy-PC.(none)>
2013-03-20 17:00:35 -0400

    Added Tidy checker to the BasicHTMLCheck class

--- SE362Project1/src/se362project1/BasicHTMLParser.java
+++ SE362Project1/src/se362project1/BasicHTMLParser.java
@@ -5,12 +5,22 @@
 
 package se362project1;
 
-import javax.swing.text.html.parser.*;
+import org.w3c.tidy.*;
 
 /**
  *
- * @author Randy
+ * @author Randy Goodman
  */
-public class BasicHTMLParser {
+public class BasicHTMLParser implements FormatCheck {
+    Tidy tidy = new Tidy();
+
+    @Override
+    public boolean Check(HTMLBuffer x) {
+        tidy.setXHTML(true);
+        
+        
+        
+        return false;
+    }
     
 }
