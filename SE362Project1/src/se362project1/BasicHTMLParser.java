d1bc8f698297d36cb4e03ab11b79593eaced5c14
RGood <Randy@Randy-PC.(none)>
2013-03-20 16:28:13 -0400

    BasicHTMLParser class created

---
+++ SE362Project1/src/se362project1/BasicHTMLParser.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package se362project1;
+
+import javax.swing.text.html.parser.*;
+
+/**
+ *
+ * @author Randy
+ */
+public class BasicHTMLParser {
+    
+}
