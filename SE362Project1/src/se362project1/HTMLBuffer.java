8a4887f5a2ae9d68249a5c6c0b7d029d95d427f4
RGood <Randy@Randy-PC.(none)>
2013-04-17 01:36:46 -0400

    Removing unnecessary stuff from HTMLBuffer

--- SE362Project1/src/se362project1/HTMLBuffer.java
+++ SE362Project1/src/se362project1/HTMLBuffer.java
@@ -10,7 +10,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Stack;
 import javax.swing.JEditorPane;
 
